21fc37e4da5aec774afc6a330d60704bcc3587da
Dimas Pratama <dmsprtmiskandar@gmail.com>
2021-06-22 12:30:55 +0700

    menambahkan method GET

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ const connection = require('./koneksi');
 exports.index = function (req, res) {
     response.ok("Aplikasi REST API ku berjalan", res);
 };
+
+// menampilkan semua data dari database
+exports.getNews = function (req, res) {
+    connection.query('SELECT * FROM news', function (error, rows, fields) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+};
--- routes.js
+++ routes.js
@@ -4,4 +4,5 @@ module.exports = function (app) {
     const json = require('./controller');
 
     app.route('/').get(json.index);
+    app.route('/news').get(json.getNews);
 }
