c137a016cd4cdcdfb36c274632ceb6e5f3c74ef2
Dimas Pratama <dmsprtmiskandar@gmail.com>
2021-06-22 12:07:29 +0700

    menambahkan file controller

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const response = require('./res.js');
-const connection = require('./koneksi.js');
+const response = require('./res');
+const connection = require('./koneksi');
 
 exports.index = function (req, res) {
     response.ok("Aplikasi REST API ku berjalan");
