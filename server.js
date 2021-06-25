b0019ff57e50d37225ba353a7cda98c1641c9160
Dimas Pratama <dmsprtmiskandar@gmail.com>
2021-06-25 20:01:40 +0700

    mengganti port server ke 3001

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ routes(app);
 // daftarkan menu dari index
 app.use('/auth', require('./middleware'));
 
-app.listen(3000, () => {
-    console.log(`Server started on port 3000`);
+app.listen(3001, () => {
+    console.log(`Server started on port 3001`);
 });
